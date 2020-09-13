ac5d83c54497b98b05db7f433cdcc1eef9501713
Ben Ezard <ezard.ben@gmail.com>
2020-09-13 23:54:54 +0100

    feat: Add publishing config (#16)
    * Add Semantic Release dependencies
    
    * Update author info
    
    * Update license to MIT
    
    * Update author email to match GitHub and npm
    
    * Add release command
    
    * Update main file
    
    * Update build command to build both CommonJS and ES2015 packages
    
    * Update entrypoints
    
    * Add description
    
    * Add keywords
    
    * Add release config
    
    * Create GitHub Actions workflow for publishing releases
    
    * Add environment variables for release
    
    * Update version
    
    * Use personal access token instead of GitHub Actions Bot's token
    
    * Replace deprecated parameter
    
    * Disable credential persistence
    
    * Fetch all commits instead of just the latest one
    
    * Remove publishing of assets

---
+++ release.config.js
@@ -0,0 +1,13 @@
+const changelogFile = 'CHANGELOG.md';
+
+module.exports = {
+  branch: 'master',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', { changelogFile }],
+    ['@semantic-release/git', { assets: [changelogFile] }],
+    ['@semantic-release/github', { assignees: 'Ezard' }],
+    '@semantic-release/npm',
+  ],
+};
