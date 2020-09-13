134456cc954e3d903203bbcafb80eb7ac397ad07
Ezard <ezard.ben@gmail.com>
2020-09-13 23:51:20 +0100

    Remove publishing of assets

--- release.config.js
+++ release.config.js
@@ -7,26 +7,7 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     ['@semantic-release/changelog', { changelogFile }],
     ['@semantic-release/git', { assets: [changelogFile] }],
-    [
-      '@semantic-release/github',
-      {
-        assignees: 'Ezard',
-        assets: [
-          {
-            path: 'dist/**/!(*.tsbuildinfo)',
-            label: 'All Assets',
-          },
-          {
-            path: 'dist/cjs/**/!(*.tsbuildinfo)',
-            label: 'CommonJS Assets',
-          },
-          {
-            path: 'dist/esm/**/!(*.tsbuildinfo)',
-            label: 'ES2015 Assets',
-          },
-        ],
-      },
-    ],
+    ['@semantic-release/github', { assignees: 'Ezard' }],
     '@semantic-release/npm',
   ],
 };
