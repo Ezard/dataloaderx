b4142b6957b5f0d83aeda53be7dea98a306ba7d2
Ezard <ezard.ben@gmail.com>
2020-09-13 16:00:09 +0100

    Add test to verify that ArrayDataLoader correctly handles entities being returned in the incorrect order

--- src/array-data-loader.spec.ts
+++ src/array-data-loader.spec.ts
@@ -55,4 +55,22 @@ describe('ArrayDataLoader', () => {
 
     expect(entities.length).toEqual(0);
   });
+
+  it('should handle entities being returned in a different order to that in which the IDs were supplied', async () => {
+    const arrayDataLoader = new ArrayDataLoader(
+      async () => [
+        { id: 3, userId: 2 },
+        { id: 2, userId: 1 },
+        { id: 1, userId: 2 },
+      ],
+      result => result.userId,
+    );
+    const dataLoader = arrayDataLoader.getDataLoader({});
+    const [a, b] = await Promise.all([dataLoader.load(1), dataLoader.load(2)]);
+
+    const aUserIds = a.map(item => item.userId);
+    const bUserIds = b.map(item => item.userId);
+    expect(aUserIds).toEqual([1]);
+    expect(bUserIds).toEqual([2, 2]);
+  });
 });
